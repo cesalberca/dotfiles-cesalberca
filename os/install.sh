3c32114e35b09807c820ca51800e7b3581408959
cesalberca <cesar@cesalberca.com>
2025-10-29 16:44:31 +0100

    Update commands

--- os/install.sh
+++ os/install.sh
@@ -1,14 +1,14 @@
 # Create hushlogin
 touch ~/.hushlogin
 
-# Install sdkman
-curl -s "https://get.sdkman.io" | bash
-
 # Install oh my zsh
 if [[ ! -d ~/.oh-my-zsh ]]; then
     export RUNZSH=no
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+# Add 1password SSH keys
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
 # Change MacOS configuration
 source $HOME/.dotfiles/plugins/dotfiles-cesalberca/os/.macos
